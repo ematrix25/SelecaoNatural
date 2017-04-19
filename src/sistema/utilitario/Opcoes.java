df546733b2d8608abfc038339c8a3ea9b86aed1d
Emanuel <ematrix25@gmail.com>
2017-04-19 12:58:41 -0300

    Classe Opcoes para mudar configuracoes

---
+++ src/sistema/utilitario/Opcoes.java
@@ -0,0 +1,14 @@
+package sistema.utilitario;
+
+import java.awt.Toolkit;
+
+/**
+ * Classe das opções do jogador
+ * 
+ * @author Emanuel
+ */
+public class Opcoes {
+	public static boolean controlePorMouse = true;
+	public static int larguraDaResolucao = Toolkit.getDefaultToolkit().getScreenSize().width;
+	public static int alturaDaResolucao = Toolkit.getDefaultToolkit().getScreenSize().height;
+}
--- src/sistema/utilitario/Resolucao.java
+++ src/sistema/utilitario/Resolucao.java
@@ -1,33 +1,16 @@
 package sistema.utilitario;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 /**
  * Calcula a resolução da tela relativa a resolucao do computador
  * 
  * @author Emanuel
  */
 public class Resolucao {
-	private static int larguraDaComp;
-	private static int alturaDaComp;
-
-	public Resolucao() {
-		Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();
-		larguraDaComp = dimensao.width;
-		alturaDaComp = dimensao.height;
-	}
-
-	public Resolucao(int largura, int altura) {
-		larguraDaComp = largura;
-		alturaDaComp = altura;
-	}
-
 	public static int calcLarguraRelativa(int altura) {
-		return (larguraDaComp * altura) / alturaDaComp;
+		return (Opcoes.larguraDaResolucao * altura) / Opcoes.alturaDaResolucao;
 	}
 
 	public static int calcAlturaRelativa(int largura) {
-		return (alturaDaComp * largura) / larguraDaComp;
+		return (Opcoes.alturaDaResolucao * largura) / Opcoes.larguraDaResolucao;
 	}
 }
